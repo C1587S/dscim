357ca2150cb71e8ef8fb1c6d1bd0e0d26a7cb704
sebastian-cs <s.cadavid1587@gmail.com>
2025-11-26 04:16:59 -0600

    equity: add damages_calculation method returning xarray Dataset

--- src/dscim/menu/equity.py
+++ src/dscim/menu/equity.py
@@ -72,6 +72,46 @@ class EquityRecipe(MainRecipe):
 
         return df
 
+    def damages_calculation(self, geography) -> xr.Dataset:
+        """Aggregate damages to country/region level
+
+        Returns
+        --------
+            xr.Dataset with damages variable
+        """
+        self.logger.info(f"Calculating damages for geography: {geography}")
+
+        # Get damages from equity (returns as xarray)
+        dams_collapse = self.calculated_damages
+
+        if geography == "ir":
+            pass
+        elif geography == "country":
+            territories = []
+            mapping_dict = {}
+            for ii, row in self.countries_mapping.iterrows():
+                mapping_dict[row["ISO"]] = row["MatchedISO"]
+                if row["MatchedISO"] == "nan":
+                    mapping_dict[row["ISO"]] = "nopop"
+
+            for region in dams_collapse.region.values:
+                    territories.append(mapping_dict[region[:3]])
+
+            dams_collapse = (dams_collapse
+                             .assign_coords({'region':territories})
+                             .groupby('region')
+                             .sum())
+        elif geography == "globe":
+            dams_collapse = dams_collapse.sum(dim="region").assign_coords({'region':'globe'}).expand_dims('region')
+
+        if "gwr" in self.discounting_type:
+            dams_collapse = dams_collapse.assign(
+                ssp=str(list(self.gdp.ssp.values)),
+                model=str(list(self.gdp.model.values)),
+            )
+
+        return dams_collapse.to_dataset(name = 'damages')
+
     def global_consumption_calculation(self, disc_type):
         # get global consumption certainty equivalent across regions
         ce_cons = c_equivalence(
